a29d15ff51a55cd711718c542ee096e540fa660a
VietHoan9 <hoanvieth2992003@gmail.com>
2024-09-29 21:43:45 +0700

    sửa lần 2

--- BTVN_buoi3/main.py
+++ BTVN_buoi3/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from numpy import array
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,36 +7,31 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('diemPython.csv')
 
 # Chuyển dữ liệu thành mảng numpy
-in_data = df.to_numpy()
+in_data = array(df.iloc[:,:])
+print(in_data)
 
 # Tính tổng số sinh viên đi thi
-total_students = len(in_data[:, 1])
-print('Tổng số sinh viên đi thi:', total_students)
+total_students = in_data[:, 2]
+print('Tổng số sinh viên đi thi:', np.sum(total_students))
 
 # Tìm lớp có số sinh viên đạt nhiều điểm A nhất
-diem_A = df[df['Đánh giá điểm'] == 'A']
-diem_A_by_lop = diem_A['Lớp'].value_counts()
-lop_max_diem_A = diem_A_by_lop.idxmax()
-so_sv_max_diem_A = diem_A_by_lop.max()
+diemA = in_data[:, 4]
+diemBc = in_data[:, 5]
+lops = in_data[:, 1]
+max_diemA = diemA.max()
+max_diemA_index = np.where(diemA == max_diemA)
+print('Lớp có nhiều sinh viên đạt điểm A nhất là {max_diemA_index} với {max_diemA} sinh viên đạt điểm A')
 
-print(f'Lớp có số sinh viên đạt nhiều điểm A nhất là {lop_max_diem_A} với {so_sv_max_diem_A} sinh viên đạt điểm A.')
+# Vẽ biểu đồ phân bố điểm A, B+
 
-# Lọc dữ liệu theo lớp
-df_K15 = df[df['Lớp'] == 'K15']
-df_K16 = df[df['Lớp'] == 'K16']
-
-# Đếm số lượng sinh viên theo đánh giá điểm
-data_K15 = df_K15['Đánh giá điểm'].value_counts().sort_index()
-data_K16 = df_K16['Đánh giá điểm'].value_counts().sort_index()
-
-# Tạo biểu đồ đường
-plt.figure(figsize=(12, 6))
-plt.plot(data_K15.index, data_K15.values, label='Lớp K15', marker='o')
-plt.plot(data_K16.index, data_K16.values, label='Lớp K16', marker='^')
+plt.figure(figsize=(10, 6))
+plt.plot(lops, diemA, label="Điểm A", marker='o')
+plt.plot(lops, diemBc, label="Điểm B+", marker='^')
 
 plt.xlabel('Lớp')
 plt.ylabel('Số sinh viên đạt điểm')
-plt.legend(loc='upper right')
-plt.title('Biểu đồ phân bố điểm theo lớp')
+plt.legend()
+plt.title('Biểu đồ phân bố điểm A, B+ theo lớp')
 plt.grid(True)
+plt.xticks(rotation=45)  # Xoay nhãn trục x để dễ đọc hơn
 plt.show()
